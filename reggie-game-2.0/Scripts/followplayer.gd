d40d01afeaeb4fad55bb31f68181f454bd5579c6
Matercan <dreellisgaming@gmail.com>
2025-05-04 12:35:28 +0200

    Camera-Catch up
    Camera now catches up to reggie

--- reggie-game-2.0/Scripts/followplayer.gd
+++ reggie-game-2.0/Scripts/followplayer.gd
@@ -2,16 +2,31 @@ extends Node2D
 
 @export var reggie : Node2D
 @export var max_velocity = 90.0
+@export var max_distance = 165
+@onready var fast_move_mode: bool = false
+@export var max_fast_speed_velocity: float = 180.0
 	
 func calculate_new_pos(start_vector : Vector2, end_vector :Vector2, delta: float) -> Vector2:
+	var truevelocity: float
+	if fast_move_mode:
+		truevelocity = max_fast_speed_velocity
+	else: truevelocity = max_velocity
+	
 	var direction = end_vector - start_vector
 	var distance = direction.length()
 	
-	if distance <= max_velocity * delta:
+	if distance >= max_distance:
+		print("exceeded max distance")
+		fast_move_mode = true
+	if (distance < 10) and fast_move_mode:
+		fast_move_mode = false
+		print("Caught up with end vector")
+		
+	if distance <= truevelocity * delta:
 		return end_vector
 	else:
 		print("I think something happened here")
-		return start_vector + direction.normalized() * max_velocity * delta
+		return start_vector + direction.normalized() * truevelocity * delta
 	
 func _physics_process(delta):
 	var new_pos = calculate_new_pos(global_position, reggie.global_position, delta)
--- reggie-game-2.0/Scripts/gun.gd
+++ reggie-game-2.0/Scripts/gun.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
 @export var guntip: Sprite2D
+@export var rotation_speed_deg: float = 180.0  # Degrees per second
 
 func _physics_process(delta: float) -> void:
-	look_at(get_global_mouse_position())
+	var target_pos = get_global_mouse_position()
+	var to_mouse = (target_pos - global_position).normalized()
+	var current_facing = Vector2.RIGHT.rotated(rotation)  # Local right direction
+	var max_angle = deg_to_rad(rotation_speed_deg * delta)
+
+	var new_direction = rotate_toward_limited(current_facing, to_mouse, max_angle)
+	rotation = new_direction.angle()
 	
 	if Input.is_action_just_pressed("shoot"): shoot()
+	
+
+func rotate_toward_limited(from: Vector2, to: Vector2, max_angle: float) -> Vector2:
+	if from == Vector2.ZERO or to == Vector2.ZERO:
+		return from
+	var angle_to_target = from.angle_to(to)
+	var clamped_angle = clamp(angle_to_target, -max_angle, max_angle)
+	return from.rotated(clamped_angle)
 
 func shoot() -> void:
 	var gunshot: AudioStreamPlayer2D = $Gunshot
